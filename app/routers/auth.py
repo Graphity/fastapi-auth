f2bb0a18c22b13c53639cec338e49107fb247a4a
Graphity <rohood3@gmail.com>
2023-01-15 23:59:43 +0400

    Change auth requirements to username and password

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from app.schemas.token import Token
 from app.dependencies import get_db
-from app.crud.user import get_by_email
+from app.crud.user import get_by_username
 from app.core.security import create_access_token, check_password
 
 router = APIRouter(tags=["Authentication"])
@@ -12,10 +12,10 @@ router = APIRouter(tags=["Authentication"])
 
 @router.post("/login", response_model=Token)
 def login_user(user_credentials: OAuth2PasswordRequestForm = Depends(), db_session: Session = Depends(get_db)):
-    user = get_by_email(db_session, user_credentials.username)
+    user = get_by_username(db_session, user_credentials.username)
 
     if not user:
-        raise HTTPException(status_code=403, detail="Invalid Email")
+        raise HTTPException(status_code=403, detail="Invalid Username")
 
     if not check_password(user_credentials.password, user.password):
         raise HTTPException(status_code=403, detail="Invalid Password")
@@ -23,4 +23,3 @@ def login_user(user_credentials: OAuth2PasswordRequestForm = Depends(), db_sessi
     access_token = create_access_token(data={"sub": user.username})
 
     return {"access_token": access_token, "token_type": "bearer"}
-
